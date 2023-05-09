cc3ec1da6567410f8c5e235e5be56890b00d5799
Dre <bryodiiidah@gmail.com>
2023-05-09 14:40:20 +0000

    update

---
+++ Example.svelte
@@ -0,0 +1,19 @@
+<script>
+import {useEffect} from "./useEffect";
+
+let count = 0;
+
+function handleAddCount(){
+    count  = count + 1;
+}
+
+function logUpdate(){
+    console.log("count changed to " + count );
+}
+
+useEffect(logUpdate, ()=>[count]);
+</script>
+
+<button on:click={handleAddCount}>
+    Count is {count}. Click to add
+</button>
